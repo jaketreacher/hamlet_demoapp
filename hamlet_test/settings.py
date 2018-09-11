d708fef98aad3ad2a39c5e678ae6da1fb2b3b42b
Jake Treacher <git@jaketreacher.com>
2018-09-11 20:53:45 +0800

    Update `ALLOWED_HOSTS`

--- hamlet_test/settings.py
+++ hamlet_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'very_secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.33.10', 'localhost']
+ALLOWED_HOSTS = ['hamlet-test', '192.168.33.10', 'localhost']
 
 
 # Application definition
