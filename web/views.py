be76dd81a672885cf0f69dc253557d95c6df6951
Jake Treacher <git@jaketreacher.com>
2018-09-21 22:09:37 +0800

    Add static files and display database information

--- web/views.py
+++ web/views.py
@@ -1,11 +1,35 @@
 from os import popen
-from django.http import HttpResponse
+from django.conf import settings
+from django.shortcuts import render
+
 
 def home(request):
+    context = {
+        "branch": get_branch(),
+        "database": get_database()
+    }
+    return render(request, "web/index.html", context=context)
+
+def get_branch():
     result = popen("git branch").read()
     result = result.replace(" ", "").split("\n")[0:-1] # last result is empty
     try:
         branch = [r for r in result if "*" == r[0]][0][1:]
-        return HttpResponse("Current branch: {}".format(branch))
+        return branch
     except Exception as e:
-        return HttpResponse("Error finding branch.")
+        return None
+
+
+def get_database():
+    database = {}
+    if 'postgresql' in settings.DATABASES['default']['ENGINE']:
+        database = {
+            'engine': 'postgresql',
+            'name': settings.DATABASES['default']['NAME']
+        }
+    else:
+        database = {
+            'engine': 'sqlite3',
+            'name': None
+        }
+    return database
